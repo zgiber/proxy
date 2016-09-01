d2168445ec98075d2995c880c782a0b9d9e2a3f5
Zoltan Giber <zoltan@typeform.com>
2016-09-01 15:01:34 +0200

    chain directors

--- chain.go
+++ chain.go
@@ -1,2 +1,21 @@
 package proxy
 
+import "net/http"
+
+func ChainDirectors(directors ...func(*http.Request)) func(*http.Request) {
+	return func(req *http.Request) {
+
+		ctx := req.Context()
+		if ctx.Err() != nil {
+			return
+		}
+
+		for _, director := range directors {
+			director(req)
+			if req.Context().Err() != nil {
+				return
+				// error is handled by the RoundTripper
+			}
+		}
+	}
+}
