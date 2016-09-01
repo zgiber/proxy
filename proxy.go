978e91673d5c192835f16b324dda2b7eb20ca282
Zoltan Giber <zoltan@typeform.com>
2016-09-01 17:23:12 +0200

    exposed config api

--- proxy.go
+++ proxy.go
@@ -13,17 +13,51 @@ type roundTripper struct {
 
 // ReverseProxy is the same as httputil.ReverseProxy
 // except that it uses a wrapped Transport, which
-// handles errors created by a Director
+// handles errors created by a Director.
 type ReverseProxy struct {
 	*httputil.ReverseProxy
+	configAPI *http.ServeMux
 }
 
 func New() *ReverseProxy {
+
 	return &ReverseProxy{
 		&httputil.ReverseProxy{
 			Transport: newRoundTripper(http.DefaultTransport),
 		},
+		http.NewServeMux(),
 	}
+}
+
+// AddDirector registers a director to be chained after the existing
+// proxy director.
+func (rp *ReverseProxy) AddDirector(director func(req *http.Request)) {
+	rp.Director = ChainDirectors(rp.Director, director)
+}
+
+// AddDynamicDirector registers a director on the reverseproxy and
+// registers the given http.Handlers on the configAPI http server.
+// This way we can provide a http configuration interface for
+// directors to be changed/configured on the fly.
+func (rp *ReverseProxy) AddDynamicDirector(
+	pattern string,
+	configAPIHandler http.Handler,
+	director func(req *http.Request),
+) {
+	rp.configAPI.Handle(pattern, configAPIHandler)
+	rp.Director = ChainDirectors(rp.Director, director)
+}
+
+// ListenAndServeConfigAPI starts the http server for the configuration
+// interface on the given addr.
+func (rp *ReverseProxy) ListenAndServeConfigAPI(addr string) error {
+	return http.ListenAndServe(addr, rp.configAPI)
+}
+
+// ListenAndServeConfigAPI starts the https server for the configuration
+// interface on the given addr.
+func (rp *ReverseProxy) ListenAndServeConfigAPITLS(addr, certFile, keyFile string) error {
+	return http.ListenAndServeTLS(addr, certFile, keyFile, rp.configAPI)
 }
 
 func newRoundTripper(t http.RoundTripper) http.RoundTripper {
--- server/main.go
+++ server/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http/httputil"
+	"net/url"
 
+	"github.com/zgiber/proxy"
+)
+
+func main() {
+	reverseProxy := proxy.New()
+	reverseProxy.ReverseProxy = httputil.NewSingleHostReverseProxy(
+		&url.URL{
+			Scheme: "http",
+			Host:   "localhost",
+		})
 }
