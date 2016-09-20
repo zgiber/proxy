5d1f7f581375a9310c2de4b57c35944b5448de2b
Zoltan Giber <zoltan@typeform.com>
2016-09-20 17:31:30 +0200

    small fixes

--- directors/directors.go
+++ directors/directors.go
@@ -1,18 +1,19 @@
 package directors
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
-// RewriteRequest replaces the url in the request with the provided target
+// rewriteRequest replaces the url in the request with the provided target
 // The followings are considered:
 // target.Scheme, target.Host, target.Path
 // Empty values on the target are ignored.
-func RewriteRequest(target *url.URL, req *http.Request) {
-
+func rewriteRequest(target *url.URL, req *http.Request) {
 	if target.Scheme != "" {
+		// fmt.Println(target.Scheme)
 		req.URL.Scheme = target.Scheme
 	}
 
@@ -21,22 +22,26 @@ func RewriteRequest(target *url.URL, req *http.Request) {
 	}
 
 	if target.Path != "" {
-		RewriteRequestPath(target.Path, req)
+		rewriteRequestPath(target.Path, req)
 	}
 
+	//req.URL.Path = SingleJoiningSlash(target.Path, req.URL.Path)
 	if _, ok := req.Header["User-Agent"]; !ok {
 		// explicitly disable User-Agent so it's not set to default value
 		req.Header.Set("User-Agent", "")
 	}
+
+	fmt.Println("target Scheme", target.Scheme)
+	fmt.Println("request Scheme", req.URL.Scheme)
 }
 
-// RewriteRequestPath changes the request's path to the target path.
+// rewriteRequestPath changes the request's path to the target path.
 // The target path may have variables defined e.g.: "/users/:user_id/details"
 // In this case the request context is expected to have a value for the
 // key 'user_id'.
 //
 // TODO: If there is no such value.
-func RewriteRequestPath(targetPath string, req *http.Request) {
+func rewriteRequestPath(targetPath string, req *http.Request) {
 	pathSegments := strings.Split(strings.Trim(targetPath, "/"), "/")
 
 	for i := 0; i < len(pathSegments); i++ {
@@ -54,12 +59,14 @@ func RewriteRequestPath(targetPath string, req *http.Request) {
 
 	newPath := strings.Join(pathSegments, "/")
 
-	req.URL.Path = newPath
+	req.URL.Path = "/" + newPath
+	req.URL.RawPath = req.URL.EscapedPath()
+
 }
 
-// SingleJoiningSlash properly joins two paths
+// singleJoiningSlash properly joins two paths
 // handling trailing and leading '/' characters.
-func SingleJoiningSlash(a, b string) string {
+func singleJoiningSlash(a, b string) string {
 	aslash := strings.HasSuffix(a, "/")
 	bslash := strings.HasPrefix(b, "/")
 	switch {
