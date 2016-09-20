e97c3851cdc3db59205a85b4fdf4d040af62237f
Zoltan Giber <zoltan@typeform.com>
2016-09-20 13:18:48 +0200

    router is functional

--- directors/router.go
+++ directors/router.go
@@ -17,25 +17,49 @@ type routeTree struct {
 }
 
 func NewRouterDirector(targets map[string]func(*http.Request)) func(req *http.Request) {
-	routes := buildRouteTree(targets)
-	_ = routes
+	rt := buildRouteTree(targets)
 
 	return func(req *http.Request) {
-		if director, ok := matchRoute(req.URL.Path, targets); ok {
+		if director, ok := rt.matchRoute(req.URL.Path); ok {
 			director(req)
 		} else {
-			// raise error
-			err := errors.New("invalid target")
-			ctx := context.WithValue(req.Context(), "error", err)
-			ctx, cancel := context.WithCancel(ctx)
-			cancel()
-			*req = *req.WithContext(ctx)
+			cancelRequestWithError(req, errors.New("invalid target"))
 		}
 	}
 }
 
-func matchRoute(requestPath string, targets map[string]func(*http.Request)) (func(*http.Request), bool) {
-	return nil, false
+// matchRoute finds the route for a given path
+// variable values defined in the route by ":key" syntax
+// are applied to the request context by wrapping the director.
+func (rt *routeTree) matchRoute(path string) (func(*http.Request), bool) {
+	pathSegments := strings.Split(strings.Trim(path, "/"), "/")
+	var match bool
+	variables := map[string]string{}
+	currentNode := rt
+
+	for _, pathSegment := range pathSegments {
+		currentNode, match = currentNode.matchSegment(pathSegment)
+		if !match {
+			return nil, false
+		}
+
+		if strings.HasPrefix(currentNode.route, ":") {
+			key := currentNode.route[1:]
+			variables[key] = pathSegment
+		}
+	}
+
+	director := directorWithVariables(currentNode.director, variables)
+	return director, match
+}
+
+func (rt *routeTree) matchSegment(segment string) (*routeTree, bool) {
+	next, ok := rt.children[segment]
+	if !ok {
+		next, ok = rt.children["*"]
+	}
+
+	return next, ok
 }
 
 func newRouteTree(route string) *routeTree {
@@ -50,8 +74,7 @@ func newRouteTree(route string) *routeTree {
 }
 
 func buildRouteTree(targets map[string]func(*http.Request)) *routeTree {
-	rt := &routeTree{
-		route:    "/",
+	root := &routeTree{
 		children: map[string]*routeTree{},
 	}
 
@@ -61,24 +84,48 @@ func buildRouteTree(targets map[string]func(*http.Request)) *routeTree {
 		}
 
 		path := strings.Split(strings.Trim(routeDefinition, "/"), "/")
-		root := rt
+		currentNode := root
+
 		var child *routeTree
+		var ok bool
 		for _, pathSegment := range path {
-			child, ok := root.children[pathSegment]
+
+			child, ok = currentNode.children[pathSegment]
 			if !ok {
 				child = newRouteTree(pathSegment)
 			}
-			root.children[pathSegment] = child
+			if strings.HasPrefix(pathSegment, ":") {
+				currentNode.children["*"] = child
+			} else {
+				currentNode.children[pathSegment] = child
+			}
+			currentNode = child
 		}
 
-		// set director on last pathSegment
+		// set director on the final pathSegment (full match)
 		child.director = target
 	}
 
-	return nil
+	return root
+}
+
+func directorWithVariables(director func(*http.Request), variables map[string]string) func(*http.Request) {
+	return func(req *http.Request) {
+		for key, value := range variables {
+			setRequestVariable(req, key, value)
+		}
+		director(req)
+	}
+}
+
+func setRequestVariable(req *http.Request, key, value string) {
+	*req = *req.WithContext(context.WithValue(req.Context(), key, value))
 }
 
 func routePathIsValid(path string) bool {
+	// validate path here
+	// If it contains * it must be the last character
+	// TODO: better use of wildcards (allow surrounded wildcard etc.)
 	return true
 }
 
---
+++ directors/router_test.go
@@ -0,0 +1,47 @@
+package directors
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestMatchRoutes(t *testing.T) {
+
+	incomingPaths := []string{
+		"/segment1/segment2/segment3", // simple route
+		"/segment2/segment3/",         // ending in '/'
+		"/segment3/user123/resource",  // expected to match wildcard
+		"/nomatch/",                   // no match
+		"/nomatch",                    // no match
+		"/",                           // root
+	}
+
+	results := []interface{}{}
+	expected := []interface{}{1, 2, "user123", -1, -1, 4}
+
+	targets := map[string]func(*http.Request){
+		"/segment1/segment2/segment3": func(req *http.Request) { results = append(results, 1) },
+		"/segment2/segment3":          func(req *http.Request) { results = append(results, 2) },
+		"/segment3/:user_id/resource": func(req *http.Request) { results = append(results, req.Context().Value("user_id")) },
+		"/": func(req *http.Request) { results = append(results, 4) },
+	}
+
+	rt := buildRouteTree(targets)
+	req := &http.Request{}
+
+	for _, path := range incomingPaths {
+		d, ok := rt.matchRoute(path)
+		if !ok {
+			results = append(results, -1)
+			continue
+		}
+
+		d(req)
+	}
+
+	for i, result := range results {
+		if result != expected[i] {
+			t.Fatal("Invalid result from director")
+		}
+	}
+}
