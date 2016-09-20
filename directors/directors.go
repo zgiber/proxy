2d75d25dbd4a6a66dbe259add4769cbbcf486de0
Zoltan Giber <zoltan@typeform.com>
2016-09-20 14:51:43 +0200

    url rewrite is cleaned

--- directors/directors.go
+++ directors/directors.go
@@ -1,30 +1,60 @@
 package directors
 
 import (
-	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
-// RewriteRequest replaces target url in the request with the provided one
+// RewriteRequest replaces the url in the request with the provided target
+// The followings are considered:
+// target.Scheme, target.Host, target.Path
+// Empty values on the target are ignored.
 func RewriteRequest(target *url.URL, req *http.Request) {
-	fmt.Println("DEBUG - rewriteRequest")
-	targetQuery := target.RawQuery
-	req.URL.Scheme = target.Scheme
-	req.URL.Host = target.Host
-	req.URL.Path = SingleJoiningSlash(target.Path, req.URL.Path)
-	if targetQuery == "" || req.URL.RawQuery == "" {
-		req.URL.RawQuery = targetQuery + req.URL.RawQuery
-	} else {
-		req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+
+	if target.Scheme != "" {
+		req.URL.Scheme = target.Scheme
 	}
+
+	if target.Host != "" {
+		req.URL.Host = target.Host
+	}
+
+	if target.Path != "" {
+		RewriteRequestPath(target.Path, req)
+	}
+
 	if _, ok := req.Header["User-Agent"]; !ok {
 		// explicitly disable User-Agent so it's not set to default value
 		req.Header.Set("User-Agent", "")
 	}
-	fmt.Println("DEBUG - rewriteRequest DONE")
-	fmt.Println(req.URL.Scheme, req.URL.Host, req.URL.RawPath, req.URL.RawQuery)
+}
+
+// RewriteRequestPath changes the request's path to the target path.
+// The target path may have variables defined e.g.: "/users/:user_id/details"
+// In this case the request context is expected to have a value for the
+// key 'user_id'.
+//
+// TODO: If there is no such value.
+func RewriteRequestPath(targetPath string, req *http.Request) {
+	pathSegments := strings.Split(strings.Trim(targetPath, "/"), "/")
+
+	for i := 0; i < len(pathSegments); i++ {
+		segment := pathSegments[i]
+		if strings.HasPrefix(segment, ":") {
+			value := req.Context().Value(segment[1:])
+			if stringValue, ok := value.(string); ok {
+				pathSegments[i] = stringValue
+			}
+		}
+		if segment == "+" {
+			pathSegments[i] = strings.Trim(req.URL.Path, "/")
+		}
+	}
+
+	newPath := strings.Join(pathSegments, "/")
+
+	req.URL.Path = newPath
 }
 
 // SingleJoiningSlash properly joins two paths
